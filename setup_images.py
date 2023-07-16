05a61950d9ce79bafaa414c5f79759047ab7c8e4
ss0201 <ss0201.777@gmail.com>
2023-07-16 19:47:07 +0900

    refactor: Refactor setup_images.py for better readability and maintainability.

--- setup_images.py
+++ setup_images.py
@@ -8,6 +8,8 @@ from pathlib import Path
 
 from PIL import Image, ImageChops, ImageOps
 
+UNMATCHED_DIR_NAME = "_unmatched"
+
 
 def calc_image_similarity(file1, file2):
     im1 = Image.open(file1)
@@ -20,40 +22,57 @@ def calc_image_similarity(file1, file2):
     return zero_diff_ratio
 
 
-def find_matching_train_dir(file_path, train_dirs):
+def find_matching_train_dir(file_path, train_dirs, similarity_threshold):
     for train_dir in train_dirs:
         train_file_path = os.path.join(train_dir, os.path.basename(file_path))
         if os.path.exists(train_file_path):
             similarity = calc_image_similarity(file_path, train_file_path)
-            if similarity > 0.5:
+            if similarity > similarity_threshold:
+                logging.info(
+                    f"Found matching file {train_file_path} (similarity: {similarity})"
+                )
                 return train_dir
 
     for train_dir in train_dirs:
         for train_file in os.listdir(train_dir):
             train_file_path = os.path.join(train_dir, train_file)
             similarity = calc_image_similarity(file_path, train_file_path)
-            if similarity > 0.5:
+            if similarity > similarity_threshold:
+                logging.info(
+                    f"Found matching file {train_file_path} (similarity: {similarity})"
+                )
                 return train_dir
     return None
 
 
-def copy_src_files_to_work_dir(work_dir, src_dir, train_dirs):
-    for file in os.listdir(src_dir):
-        logging.info(f"Processing {file}...")
+def copy_src_files_to_work_dir(work_dir, src_dir, train_dirs, similarity_threshold):
+    for src_file in os.listdir(src_dir):
+        if any(
+            os.path.exists(
+                os.path.join(work_dir, os.path.basename(train_dir), src_file)
+            )
+            for train_dir in train_dirs + [UNMATCHED_DIR_NAME]
+        ):
+            logging.info(f"Skipping {src_file}...")
+            continue
 
-        file_path = os.path.join(src_dir, file)
-        matching_train_dir = find_matching_train_dir(file_path, train_dirs)
+        logging.info(f"Processing {src_file}...")
+
+        src_file_path = os.path.join(src_dir, src_file)
+        matching_train_dir = find_matching_train_dir(
+            src_file_path, train_dirs, similarity_threshold
+        )
         dest_dir_name = (
-            "_unmatched"
+            UNMATCHED_DIR_NAME
             if matching_train_dir is None
             else os.path.basename(matching_train_dir)
         )
-        dest_path = os.path.join(work_dir, dest_dir_name, file)
+        dest_path = os.path.join(work_dir, dest_dir_name, src_file)
 
-        logging.info(f"Copying {file_path} to {dest_path}...")
+        logging.info(f"Copying {src_file_path} to {dest_path}...")
 
         os.makedirs(os.path.dirname(dest_path), exist_ok=True)
-        shutil.copy(file_path, dest_path)
+        shutil.copy2(src_file_path, dest_path)
 
 
 def main():
@@ -73,12 +92,20 @@ def main():
         "--train_dirs", type=str, nargs="+", help="Training directories.", required=True
     )
     parser.add_argument("--log_level", type=str, default="INFO", help="Log level.")
+    parser.add_argument(
+        "--threshold",
+        type=float,
+        default=0.5,
+        help="Similarity threshold for images (0-1). (default: 0.5)",
+    )
     args = parser.parse_args()
 
     logging.basicConfig(level=args.log_level.upper())
     logging.info(f"Copying files from {args.src_dir} to {args.work_dir}...")
 
-    copy_src_files_to_work_dir(Path(args.work_dir), Path(args.src_dir), args.train_dirs)
+    copy_src_files_to_work_dir(
+        Path(args.work_dir), Path(args.src_dir), args.train_dirs, args.threshold
+    )
 
     logging.info("Done.")
 
